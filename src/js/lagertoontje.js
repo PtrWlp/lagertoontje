8c79bd21b9977196e2765a2c89e0ca14eca48a1a
PtrWlp <peter.welp@klm.com>
2015-11-26 19:33:13 +0100

    wp

--- src/js/lagertoontje.js
+++ src/js/lagertoontje.js
@@ -3,13 +3,19 @@ var $allSections = $('#tl-accordion > h2');
 var $activeSection = $(location.hash);
 if ($activeSection.length !== 0) {
 	$activeSection.next().slideDown()
+	$activeSection.addClass('tl-open');
 }
 
 $('#tl-accordion > h2').click(function() {
-	$allPanels.slideUp();
-	$(this).next().slideDown();
-	// More things to do
+	var $this = $(this);
 
+	if (!$this.hasClass('tl-open')) {
+		$allPanels.slideUp();
+		$allSections.removeClass('tl-open');
+		location.hash = $this.attr('id');
+		$this.next().slideDown();
+		$this.addClass('tl-open');
+	}
 
 	return false;
 });
