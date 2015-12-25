05a81964c4f2c70106a3c3df408c1be643bff1f5
PtrWlp <peter.welp@klm.com>
2015-12-25 16:10:21 +0100

    lightbox

--- src/js/lagertoontje.js
+++ src/js/lagertoontje.js
@@ -72,3 +72,15 @@ $( document ).ready(function() {
 
 })
 
+
+$(document).ready(function() {
+    $('.image-popup-vertical-fit').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      mainClass: 'mfp-img-mobile',
+      image: {
+        verticalFit: true
+      }
+      
+    });
+});
